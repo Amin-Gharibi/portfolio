984d8089efa33f9f1d400ab9fdf4881f9b05b5db
Amin Gharibi <gharibi8364@gmail.com>
2024-03-25 21:01:37 +0330

    make header for desktop and customize tailwind base

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
         center: true
       }
     },
-    color: {
+    colors: {
       'gradient-1': "linear-gradient(to right, rgba(0, 192, 253, 1), rgba(231, 15, 170, 1))",
       'gradient-2': "linear-gradient(to right, rgba(19, 176, 245, 1), rgba(231, 15, 170, 1))",
       'gradient-3': "linear-gradient(to bottom, rgba(231, 15, 170, 1), rgba(0, 192, 253, 1))",
