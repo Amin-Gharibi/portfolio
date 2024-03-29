97585262fc6537f09f3011155c657b54440c222b
Amin Gharibi <gharibi8364@gmail.com>
2024-03-29 16:52:22 +0330

    create base of theme changing

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,33 @@
 import {initInfiniteSwiper, toggleMobileHeaderMenu} from "./utilities/utility.js";
 
+const changeThemeHandler = () => {
+	const themeToggleIcons = document.querySelectorAll('.toggle-theme use')
+	if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+		themeToggleIcons.forEach(icon => {
+			icon.setAttribute('href', '#sun-icon')
+		})
+	} else {
+		themeToggleIcons.forEach(icon => {
+			icon.setAttribute('href', '#moon-icon')
+		})
+	}
+}
+
+changeThemeHandler()
+
+const themeToggleBtns = document.querySelectorAll('.toggle-theme')
+themeToggleBtns.forEach(btn => {
+	btn.addEventListener('click', () => {
+		console.log(localStorage.theme)
+		if (localStorage.theme === 'dark') {
+			localStorage.theme = 'light'
+		} else {
+			localStorage.theme = 'dark'
+		}
+		changeThemeHandler()
+	})
+})
+
 toggleMobileHeaderMenu()
 
 initInfiniteSwiper('.tech-stack--infinite-swiper', 'left')
