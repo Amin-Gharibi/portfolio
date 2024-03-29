d5fcebc9c8cbea8adfba244a90b9fbe04abba22d
Amin Gharibi <gharibi8364@gmail.com>
2024-03-29 17:04:16 +0330

    fix dummy bugs in base of theme changing

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,31 +1,18 @@
 import {initInfiniteSwiper, toggleMobileHeaderMenu} from "./utilities/utility.js";
 
 const changeThemeHandler = () => {
-	const themeToggleIcons = document.querySelectorAll('.toggle-theme use')
 	if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-		themeToggleIcons.forEach(icon => {
-			icon.setAttribute('href', '#sun-icon')
-		})
+		localStorage.setItem('theme', 'light')
+		document.documentElement.classList.remove('dark')
 	} else {
-		themeToggleIcons.forEach(icon => {
-			icon.setAttribute('href', '#moon-icon')
-		})
+		localStorage.setItem('theme', 'dark')
+		document.documentElement.classList.add('dark')
 	}
 }
 
-changeThemeHandler()
-
 const themeToggleBtns = document.querySelectorAll('.toggle-theme')
 themeToggleBtns.forEach(btn => {
-	btn.addEventListener('click', () => {
-		console.log(localStorage.theme)
-		if (localStorage.theme === 'dark') {
-			localStorage.theme = 'light'
-		} else {
-			localStorage.theme = 'dark'
-		}
-		changeThemeHandler()
-	})
+	btn.addEventListener('click', changeThemeHandler)
 })
 
 toggleMobileHeaderMenu()
